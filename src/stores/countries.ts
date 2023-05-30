cf8438a6c91d0fe17e740e0cb153a11602260dae
Saar Davidson <davidsonsaar@gmail.com>
2023-05-30 17:27:57 +0300

    add redux-logger

--- src/stores/countries.ts
+++ src/stores/countries.ts
@@ -1,13 +1,16 @@
-import { configureStore } from '@reduxjs/toolkit'
-import { setupListeners } from '@reduxjs/toolkit/query'
-import { countryApi } from '../lib/api'
+import { configureStore } from '@reduxjs/toolkit';
+import { setupListeners } from '@reduxjs/toolkit/query';
+import { countryApi } from '../lib/api';
+import { createLogger } from 'redux-logger';
+
+const logger = createLogger();
 
 export const store = configureStore({
   reducer: {
     [countryApi.reducerPath]: countryApi.reducer,
   },
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(countryApi.middleware),
+    getDefaultMiddleware().concat(countryApi.middleware).concat(logger),
 })
 
 setupListeners(store.dispatch)
